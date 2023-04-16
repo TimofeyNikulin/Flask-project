476c63175505c609473fec87a18853ea76f2914e
TimofeyNikulin <104652883+TimofeyNikulin@users.noreply.github.com>
2023-04-16 21:36:35 +0300

    commit

--- server.py
+++ server.py
@@ -1,12 +1,9 @@
 from app import app, socket
 from app.data import db_session, api
-
-
-def create_app():
-    db_session.global_init("app/db/web.sqlite")
-    app.register_blueprint(api.blueprint)
-    socket.run(app, host="0.0.0.0", port=5000, debug=True)
+import os
 
 
 if __name__ == "__main__":
-    create_app()
+    db_session.global_init("app/db/web.sqlite")
+    app.register_blueprint(api.blueprint)
+    socket.run(app)
