cdb77a546cd76c63cc24430e95cca0c31c0e2c4d
TimofeyNikulin <104652883+TimofeyNikulin@users.noreply.github.com>
2023-09-24 22:39:03 +0300

    commit

--- app/forms/reg_form.py
+++ app/forms/reg_form.py
@@ -4,7 +4,11 @@ from wtforms.validators import InputRequired, DataRequired
 
 
 class RegForm(FlaskForm):
-    name = StringField(label="Имя:", name="name", id="name", validators=[InputRequired()])
-    surname = StringField(label="Фамилия:", name="surname", id="surname", validators=[InputRequired()])
-    email = EmailField(label="Почта:", name="email", id="email", validators=[InputRequired()])
-    password = PasswordField(label="Пароль:", name="password", id="password", validators=[InputRequired()])
+    name = StringField(label="Имя:", name="name", id="name",
+                       validators=[InputRequired()])
+    surname = StringField(label="Фамилия:", name="surname",
+                          id="surname", validators=[InputRequired()])
+    email = EmailField(label="Почта:", name="email",
+                       id="email", validators=[InputRequired()])
+    password = PasswordField(
+        label="Пароль:", name="password", id="password", validators=[InputRequired()])
